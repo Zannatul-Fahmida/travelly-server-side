3e088f85d149fa701660c0385a08e74c62828cbe
Zannatul-Fahmida <jannatulfahmida15@gmail.com>
2021-10-31 12:19:02 +0600

    booking

--- index.js
+++ index.js
@@ -47,44 +47,15 @@ async function run() {
             res.json(result)
         });
 
-        // Add bookings
-        app.post('/addBooking', (req, res) => {
-            const name = req.body.name;
-            const email = req.body.email;
-            const price = req.body.price;
-            const status = req.body.status;
-        
-            bookingCollection.insertOne({ name, email, price, status })
-              .then(result => {
-                res.send(result.insertedCount > 0)
-              })
-              .catch(error => {
-                console.log(error);
-              });
-          });
+        // POST Booking API
+        app.post('/addBooking', async (req, res) => {
+            const newBooking = req.body;
+            console.log('hit the post api', newBooking);
 
-          //All bookings
-          app.get('/allBookings', (req, res) => {
-            bookingCollection.find({})
-              .toArray((err, docs) => res.send(docs));
-          });
-
-          //Update booking
-          app.patch('/update/:id', (req, res) => {
-            bookingCollection.updateOne({ _id: ObjectId(req.params.id) },
-              {
-                $set: { status: req.body.status }
-              })
-              .then(result => res.send(result.modifiedCount > 0))
-          });
-
-          //Get bookings
-          app.get('/bookings', (req, res) => {
-            bookingCollection.find({ email: req.query.email })
-              .toArray((err, docs) => {
-                res.send(docs)
-              })
-          });
+            const result = await bookingCollection.insertOne(newBooking);
+            console.log(result);
+            res.json(result)
+        });
 
         // DELETE API
         app.delete('/tours/:id', async (req, res) => {
